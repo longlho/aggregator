f08f3d4f52dbb0504090de25fb4d1971a76e5761
Long Ho <holevietlong@gmail.com>
2015-01-26 16:54:41 -0500

    iron out some IE8 stuff

--- public/dist/main.js
+++ public/dist/main.js
@@ -96,25 +96,32 @@ function onStateChange() {
   });
 }
 
+function getTarget(e) {
+  e || (e = window.event);
+  return e.target || e.srcElement;
+}
+
 // Render default paginators
 dom.$paginators.innerHTML = renderer.renderPaginators();
 
 // Since `getElementsByClassName` does not return a real Array
-dom.$pages = Array.prototype.slice.call(dom.$paginators.getElementsByClassName('page'));
+dom.$pages = dom.$paginators.getElementsByClassName('page');
 
-// Attach event handler to paginator
-dom.$pages.forEach(function (page) {
+// Attach event handler to paginator, can't use forEach on HTMLCollectionList
+for (var i = 0; i < dom.$pages.length; i++) {
+  var page = dom.$pages[i];
   utils.addEventListener(page, 'click', function (e) {
-    state.page = +e.target.textContent || e.target.innerText;
+    var target = getTarget(e);
+    state.page = +target.textContent || target.innerText;
 
     onStateChange();
   });
-});
-
+}
 
 // Attach event handler to search input to re-search when a key is pressed
 utils.addEventListener(dom.$searchInput, 'keyup', function (e) {
-  var query = e.target.value;
+  var target = getTarget(e)
+    , query = target.value;
 
   // Don't search if it's blank
   if (!query) {
